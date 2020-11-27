f9277ce8b3ae865e419e9de659ae347123ff6b8f
tribbloid <pc175@uow.edu.au>
2020-11-27 13:46:39 -0500

    add TreeLike impl

---
+++ src/main/scala/graph/commons/util/TreeFormat.scala
@@ -0,0 +1,45 @@
+package graph.commons.util
+
+trait TreeFormat {
+
+  lazy val DENOTE = ":-"
+  lazy val WRAP = "| "
+
+  lazy val FORK = "+ "
+  lazy val LEAF = "- "
+
+  lazy val INDENT = "  "
+
+  def wText(tt: String) = new WText(tt.split('\n'))
+
+  class WText(lines: Seq[String]) {
+
+    lazy val build: String = lines.mkString("\n")
+
+    def indent(ss: String = INDENT): WText = {
+
+      new WText(lines.map(v => ss + v))
+    }
+
+    def prepend(
+        ss: String,
+        indent: String = INDENT
+    ): WText = {
+
+      val line1 = lines.headOption.map { head =>
+        ss + head
+      }.toSeq
+
+      val remainder = lines.slice(1, Int.MaxValue).map { line =>
+        indent + line
+      }
+
+      new WText(line1 ++ remainder)
+    }
+  }
+}
+
+object TreeFormat {
+
+  object Indent2 extends TreeFormat
+}
---
+++ src/main/scala/graph/commons/util/TreeLike.scala
@@ -0,0 +1,57 @@
+package graph.commons.util
+
+trait TreeLike {
+
+  def format: TreeFormat = TreeFormat.Indent2
+
+  def nodeStr: String // supports multiple lines
+
+  def children: Seq[TreeLike] = Nil
+
+  lazy val isLeaf: Boolean = children.isEmpty
+
+  final override lazy val toString: String = {
+
+    val wText = format.wText(nodeStr)
+
+    val result = if (isLeaf) {
+
+      wText.prepend(format.LEAF).build
+    } else {
+
+      val selfT = if (isLeaf) wText.prepend(format.FORK)
+      else wText.prepend(format.FORK, format.WRAP)
+
+      val childrenTProtos = children.map { child =>
+        format.wText(child.toString)
+      }
+
+      val childrenTLast = childrenTProtos.lastOption.map { tt =>
+        tt.prepend(format.DENOTE)
+      }.toSeq
+
+      val childrenTOthers = childrenTProtos.dropRight(1).map { tt =>
+        tt.prepend(format.DENOTE, format.WRAP)
+      }
+
+      (Seq(selfT) ++ childrenTOthers ++ childrenTLast)
+        .map { v =>
+          v.build
+        }
+        .mkString("\n")
+    }
+
+    result
+  }
+}
+
+object TreeLike {
+
+  case class Str(
+                  nodeStr: String,
+                  override val children: Seq[Str] = Nil,
+                ) extends TreeLike {
+
+    override val format: TreeFormat = TreeFormat.Indent2
+  }
+}
