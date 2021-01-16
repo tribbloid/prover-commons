044d65cbaf43ebebab3bab3cf2d30e69435081df
Peng Cheng <pc175@uowmail.edu.au>
2021-01-16 01:02:49 -0500

    add implicit search order & operator priority memo

--- src/main/scala/com/tribbloids/graph/commons/util/util.scala
+++ src/main/scala/com/tribbloids/graph/commons/util/util.scala
@@ -1,5 +1,37 @@
 package com.tribbloids.graph.commons
 
+/*
+Scala implicit scope search order:
+
+First look in current scope
+Implicits defined in current scope
+Explicit imports
+wildcard imports
+Same scope in other files
+Now look at associated types in
+Companion objects of a type
+Implicit scope of an argument's type (2.9.1)
+Implicit scope of type arguments (2.8.0)
+Outer objects for nested types
+Other dimensions
+ */
+
+/*
+scala operator priority
+LOWEST!
+(all letters)
+|
+^
+&
+= !
+< >
+:
++ -
+ * / %
+(all other special characters)
+HIGHEST!
+ */
+
 package object util {
 
 //  val INDENT = "  "
