e488fae084de48f3b6c5177fb289ac5295cabb68
Peng Cheng <peng.cheng@acyclic.ai>
2023-01-21 18:06:16 -0500

    dependency upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ allprojects {
 
         val scalaTestV = "3.2.3"
         testImplementation("org.scalatest:scalatest_${vs.scalaBinaryV}:${scalaTestV}")
-        testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 
         testRuntimeOnly("co.helmethair:scalatest-junit-runner:0.2.0")
     }
