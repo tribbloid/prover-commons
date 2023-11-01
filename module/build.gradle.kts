e11f9d1bd495581e8b0945b26becd1fa8644ab07
peng <peng.cheng@acyclic.ai>
2023-11-01 17:58:31 -0400

    enable splainPlugin if version exists

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -3,11 +3,4 @@ val vs = versions()
 allprojects {
     group = "ai.acyclic.prover-commons"
 
-    dependencies {
-
-        testFixturesApi("org.scalatest:scalatest_${vs.scala.binaryV}:${vs.scalaTestV}")
-
-        testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-        testRuntimeOnly("co.helmethair:scalatest-junit-runner:0.2.0")
-    }
 }
--- notebook/build.gradle.kts
+++ notebook/build.gradle.kts
@@ -4,7 +4,4 @@ dependencies {
     api(project(":prover-commons:core"))
 
     testFixturesApi(testFixtures(project(":prover-commons:core")))
-
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testRuntimeOnly("co.helmethair:scalatest-junit-runner:0.2.0")
 }
