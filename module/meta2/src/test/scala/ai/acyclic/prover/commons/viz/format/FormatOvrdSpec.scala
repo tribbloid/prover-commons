94caf376fff5361f0ee1fa28086c114f02925ce2
peng <peng.cheng@acyclic.ai>
2024-02-26 22:50:12 -0500

    Witness in prod code are removed

--- module/meta2/src/test/scala/ai/acyclic/prover/commons/viz/format/FormatOvrdSpec.scala
+++ module/meta2/src/test/scala/ai/acyclic/prover/commons/viz/format/FormatOvrdSpec.scala
@@ -2,9 +2,10 @@ package ai.acyclic.prover.commons.viz.format
 
 import ai.acyclic.prover.commons.compat.XInt
 import ai.acyclic.prover.commons.meta.ScalaReflection
+import ai.acyclic.prover.commons.refl.XInt
 import ai.acyclic.prover.commons.testlib.BaseSpec
 import ai.acyclic.prover.commons.viz.TypeViz
-import ai.acyclic.prover.commons.viz.format.FormatOvrd.{~~, SingletonName}
+import ai.acyclic.prover.commons.viz.format.FormatOvrd.{SingletonName, ~~}
 import ai.acyclic.prover.commons.viz.format.Formats0.{ClassName, TypeImpl}
 
 class FormatOvrdSpec extends BaseSpec {
