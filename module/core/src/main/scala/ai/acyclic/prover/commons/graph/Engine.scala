0ac373cb7ec94fe1160b4209270ce9d8b022fab4
peng <peng.cheng@acyclic.ai>
2024-03-09 21:51:09 -0500

    clean up

--- module/core/src/main/scala/ai/acyclic/prover/commons/graph/Engine.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/graph/Engine.scala
@@ -2,7 +2,6 @@ package ai.acyclic.prover.commons.graph
 
 import ai.acyclic.prover.commons.graph.topology.{Axiom, Lawful, Topology}
 
-import scala.language.implicitConversions
 
 trait Engine {
   self: Singleton =>
