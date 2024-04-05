6ede7d2d42f32a7727d28c84d1c7d82e8d2a3e25
peng <peng.cheng@acyclic.ai>
2024-04-04 21:43:34 -0400

    FnAsFunction1.toString is improved

--- module/core/src/main/scala/ai/acyclic/prover/commons/function/HomSystem_Imp0.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/function/HomSystem_Imp0.scala
@@ -2,12 +2,21 @@ package ai.acyclic.prover.commons.function
 
 import ai.acyclic.prover.commons.function.api.{HasMorphism, HasPoly}
 
+import scala.language.implicitConversions
+
 trait HomSystem_Imp0 extends HasMorphism with HasPoly with Serializable {
 
   final type IUB = Any
 
-  implicit def FnAsFunction1[I, R](fn: FnCompat[I, R]): I => R = { i: I =>
-    fn.apply(i)
+  case class FnAsFunction1[I, R](fn: FnCompat[I, R]) extends (I => R) {
+
+    final override def apply(v1: I): R = fn.apply(v1)
+
+    final override def toString: String = fn.toString // preserve reference transparency
+
   }
 
+  implicit def FnAsFunction1[I, R](fn: FnCompat[I, R]): I => R = {
+    FnAsFunction1[I, R](fn)
+  }
 }
