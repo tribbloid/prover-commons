5a50e4c4f421122184bbe4379656c97eff33999e
tribbloid <peng.cheng@acyclic.ai>
2024-11-20 17:56:48 -0500

    re-enable WideTyped

--- module/meta2/src/main/scala/ai/acyclic/prover/commons/viz/WideTyped.scala
+++ module/meta2/src/main/scala/ai/acyclic/prover/commons/viz/WideTyped.scala
@@ -1,22 +1,24 @@
 package ai.acyclic.prover.commons.viz
 
+import ai.acyclic.prover.commons.refl.Reflection
+
 /**
   * @param value
   *   use compiler's type inference feature to discover the non-singleton type of the value
   * @tparam TT
   *   the wide type
   */
-//case class WideTyped[TT](value: TT) {
-//
-//  type Wide = TT
-//
-//  def viz(
-//      implicit
-//      ttag: ScalaReflection.TypeTag[TT]
-//  ) = TypeViz.apply[TT]
-//}
-//
-//object WideTyped {
-//
-//  implicit def unbox[T](v: WideTyped[T]): T = v.value
-//}
+case class WideTyped[TT](value: TT) {
+
+  type Wide = TT
+
+  def viz(
+      implicit
+      ttag: Reflection.Runtime.TypeTag[TT]
+  ) = TypeViz.apply[TT]
+}
+
+object WideTyped {
+
+  implicit def unbox[T](v: WideTyped[T]): T = v.value
+}
