dd5020d9c9d7648015096ec2bf9f5530eaa6be81
tribbloid <peng.cheng@acyclic.ai>
2024-12-13 22:26:51 -0500

    ditto

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include("prover-commons")
 project(":prover-commons").projectDir = file("module")
 
 include(
+    ":prover-commons:infra",
     ":prover-commons:core",
     ":prover-commons:meta2", // should only be working for Scala 2
     ":prover-commons:spark",
