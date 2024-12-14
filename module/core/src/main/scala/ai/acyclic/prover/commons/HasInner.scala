c8e4ab53c569d97fbf8e74f0d2d9dd845d22197f
tribbloid <peng.cheng@acyclic.ai>
2024-12-13 22:28:02 -0500

    HasOuter is useless, removed

--- module/core/src/main/scala/ai/acyclic/prover/commons/HasInner.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/HasInner.scala
@@ -1,15 +1,41 @@
 package ai.acyclic.prover.commons
 
+import ai.acyclic.prover.commons.multiverse.{CanEqual, View}
+
 trait HasInner {
 
-  trait Inner extends HasOuter {
+  import HasInner.*
 
-    final val outer: HasInner.this.type = HasInner.this
+  trait _Inner extends Inner {
 
-    type This <: outer.Inner
-
-    final val inner: This = this.asInstanceOf[This]
+    final override val outer: HasInner.this.type = HasInner.this
   }
 }
 
-object HasInner {}
+object HasInner {
+
+  trait Inner extends View.Equals {
+
+    val outer: AnyRef
+
+    @transient override def canEqualProjections =
+      super.canEqualProjections :+ (CanEqual.Native.<~(outer))
+  }
+
+  object Inner {}
+
+  def outerListOf(v: Any): List[Any] = {
+
+    val self = List(v)
+
+    val outers = v match {
+      case vv: Inner =>
+        val outer = vv.outer
+        outerListOf(outer)
+      case _ =>
+        Nil
+    }
+
+    self ++ outers
+  }
+}
