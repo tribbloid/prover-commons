4d1e953ed25aff52d6adb289c09678c5c0678a06
tribbloid <peng.cheng@acyclic.ai>
2025-01-08 22:37:12 -0500

    shapeless changed to formless

--- module/core/build.gradle.kts
+++ module/core/build.gradle.kts
@@ -8,7 +8,10 @@ dependencies {
     api("org.scalameta:ascii-graphs_${vs.scala.binaryV}:0.1.2")
 //    api("org.typelevel:cats-effect_${vs.scala.binaryV}:3.4.5")
 
-    api("com.chuusai:shapeless_${vs.scala.binaryV}:2.3.10")
+//    api("com.chuusai:shapeless_${vs.scala.binaryV}:2.3.10")
+//    libraryDependencies += "com.bondlink" %% "formless" % "0.5.1"
+
+    api("com.bondlink:formless_${vs.scala.binaryV}:0.5.1")
 
     api("com.github.ben-manes.caffeine:caffeine:3.1.8")
 }
