5e98c8cced3380eaf24a457240763f0660041f11
Peng Cheng <peng.cheng@acyclic.ai>
2026-01-06 16:07:14 -0500

    discharge ConversionPart & test

---
+++ module/core/src/main/scala/ai/acyclic/prover/commons/multiverse/rewrite/HasConversionPart.scala
@@ -0,0 +1,67 @@
+package ai.acyclic.prover.commons.multiverse.rewrite
+
+import scala.language.implicitConversions
+
+trait HasConversionPart extends HasConversionPart.Imp1 {
+
+  /**
+    * can be chained, unlike Conversion
+    *
+    * prefer to search for missing parts in forward direction
+    */
+  trait ConversionPart[-T, +R] extends Conversion[T, R] {}
+}
+
+object HasConversionPart {
+
+  // Layered trait hierarchy for implicit priority
+  // Shorter chains are preferred through linearization
+  trait Imp4 {
+    implicit def forwardSearchView4[T, R1, R2, R3, R4, R5](v: T)(
+        implicit
+        p1: HasConversionPart#ConversionPart[T, R1],
+        p2: HasConversionPart#ConversionPart[R1, R2],
+        p3: HasConversionPart#ConversionPart[R2, R3],
+        p4: HasConversionPart#ConversionPart[R3, R4],
+        p5: HasConversionPart#ConversionPart[R4, R5]
+    ): R5 = p5.normalise(p4.normalise(p3.normalise(p2.normalise(p1.normalise(v)))))
+  }
+
+  trait Imp3 extends Imp4 {
+    implicit def forwardSearchView3[T, R1, R2, R3, R4](v: T)(
+        implicit
+        p1: HasConversionPart#ConversionPart[T, R1],
+        p2: HasConversionPart#ConversionPart[R1, R2],
+        p3: HasConversionPart#ConversionPart[R2, R3],
+        p4: HasConversionPart#ConversionPart[R3, R4]
+    ): R4 = p4.normalise(p3.normalise(p2.normalise(p1.normalise(v))))
+  }
+
+  trait Imp2 extends Imp3 {
+    implicit def forwardSearchView2[T, R1, R2, R3](v: T)(
+        implicit
+        p1: HasConversionPart#ConversionPart[T, R1],
+        p2: HasConversionPart#ConversionPart[R1, R2],
+        p3: HasConversionPart#ConversionPart[R2, R3]
+    ): R3 = p3.normalise(p2.normalise(p1.normalise(v)))
+  }
+
+  trait Imp1 extends Imp2 {
+    implicit def forwardSearchView[T, R, R2](v: T)(
+        implicit
+        left: HasConversionPart#ConversionPart[T, R],
+        right: HasConversionPart#ConversionPart[R, R2]
+    ): R2 = right.normalise(left.normalise(v))
+  }
+
+//  trait BackwardMixin[-T, +R] { self: Conversion[T, R] =>
+//
+//    implicit def backwardSearch[T0, R0 >: R](v: T0)(
+//        implicit
+//        prev: ConversionPart[T0, T]
+//    ): R0 = {
+//      normalise(prev.normalise(v))
+//    }
+//
+//  }
+}
--- module/core/src/test/scala/ai/acyclic/prover/commons/multiverse/rewrite/ConversionPartSpec.scala
+++ module/core/src/test/scala/ai/acyclic/prover/commons/multiverse/rewrite/ConversionPartSpec.scala
@@ -4,40 +4,63 @@ import ai.acyclic.prover.commons.testlib.BaseSpec
 
 class ConversionPartSpec extends BaseSpec {
 
-  import ConversionPartSpec.H1.*
-  val a = A(1)
+  describe("defined in object") {
 
-  it("can cast directly") {
-    a: B
+    import ConversionPartSpec.H1.*
+    val a = A(1)
+
+    it("can cast directly") {
+      a: B
+    }
+
+    it("can cast through chain") {
+      a: C
+      a: D
+      a: E
+      a: F
+    }
   }
 
-  it("can cast through chain") {
-    a: C
-    a: D
+  describe("defined in package") {
+
+    import ConversionPartSpecFixture.*
+    val a = A(1)
+
+    it("can cast directly") {
+      a: B
+    }
+
+    it("can cast through chain") {
+      a: C
+      a: D
+      a: E
+      a: F
+    }
   }
+
 }
 
 object ConversionPartSpec {
 
-  object H1 extends HasConversionPart {
+  trait H1 extends HasConversionPart {
 
     case class A(value: Int)
     case class B(value: Int)
     case class C(value: Int)
     case class D(value: Int)
+    case class E(value: Int)
+    case class F(value: Int)
 
     // Direct conversions
-    implicit val aToB: ConversionPart[A, B] = new ConversionPart[A, B] {
-      override def normalise(v: A): B = B(v.value)
-    }
+    implicit def aToB: ConversionPart[A, B] = (v: A) => B(v.value)
 
-    implicit val bToC: ConversionPart[B, C] = new ConversionPart[B, C] {
-      override def normalise(v: B): C = C(v.value + 1)
-    }
+    implicit val bToC: ConversionPart[B, C] = (v: B) => C(v.value + 1)
 
-    implicit val cToD: ConversionPart[C, D] = new ConversionPart[C, D] {
-      override def normalise(v: C): D = D(v.value + 2)
-    }
+    implicit lazy val cToD: ConversionPart[C, D] = (v: C) => D(v.value + 2)
+
+    implicit val dToE: ConversionPart[D, E] = (v: D) => E(v.value + 3)
+
+    implicit val eToF: ConversionPart[E, F] = (v: E) => F(v.value + 4)
   }
-
+  object H1 extends H1
 }
