822f4f8c18d5de882885c82831258f8aa203b782
Peng Cheng <peng.cheng@acyclic.ai>
2026-01-11 18:30:49 -0500

    !amend

--- module/core/src/main/scala/ai/acyclic/prover/commons/function/tracing/ConstructorImplicits.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/function/tracing/ConstructorImplicits.scala
@@ -73,4 +73,68 @@ trait ConstructorImplicits extends ConstructorCanChain {
       ???
     }
   }
+
+  implicit class ConstructorLikeOps[P, I, O](private val self: ConstructorLike[P, I, O]) {
+    // TODO: should be a view of Expr[?, I :=> O]
+
+    // beta reduction, notice that P is contravariant, and Expr[Any, I] represents a static I,
+    // so Constructor[Any, I, O] can apply on any Expr[P, I]
+    def apply[PP <: P](arg: Expr[PP, I])(
+        implicit
+        _definedAt: SrcDefinition
+    ): Expr._1[P, O] = {
+
+      ???
+    }
+
+    // enable currying, calculus of variations
+//  def liftToHigherOrder( // TODO: remove, this should happen automatically
+//      implicit
+//      _definedAt: SrcDefinition
+//  ): Constructor[Unit, I :=> O] = {
+//    ???
+//  }
+
+    // stolen form ZIO ZLayers, these are shorthands for defining parallel computation graphs, they are not necessary but can make definition shorter
+
+    trait zipLike {
+
+      def apply[I2, O2](right: ConstructorLike[P, I2, O2])(
+          implicit
+          _definedAt: SrcDefinition
+      ): ConstructorLike[P, (I, I2), (O, O2)]
+    }
+
+    object zip extends zipLike {
+      override def apply[I2, O2](right: ConstructorLike[P, I2, O2])(
+          implicit
+          _definedAt: SrcDefinition
+      ): ConstructorLike[P, (I, I2), (O, O2)] = ???
+    }
+    def <*> = zip
+
+    //  object zipPar extends zipLike {
+    //    override def apply[I2, O2](right: TracingV2[I2, O2])(
+    //      implicit
+    //      _definedAt: SrcDefinition
+    //    ): TracingV2[(I, I2), (O, O2)] = ???
+    //  }
+    //  def <&> = zipPar
+
+    object union {
+
+      def apply[I2 <: I, O2](right: ConstructorLike[P, I2, O2])(
+          implicit
+          _definedAt: SrcDefinition
+      ): ConstructorLike[P, I2, (O, O2)] = {
+        ???
+      }
+    }
+
+    object OrElse {}
+    def <> = OrElse
+
+    object OrElseEither {}
+    def <+> = OrElseEither
+  }
 }
