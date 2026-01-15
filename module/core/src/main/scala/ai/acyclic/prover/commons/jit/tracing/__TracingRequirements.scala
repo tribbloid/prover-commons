d6e354c6aa616a457cfb87a217d2c22b6829e4b4
Peng Cheng <peng.cheng@acyclic.ai>
2026-01-15 17:10:08 -0500

    !amend

--- module/core/src/main/scala/ai/acyclic/prover/commons/jit/tracing/__TracingRequirements.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/jit/tracing/__TracingRequirements.scala
@@ -23,6 +23,7 @@ object __TracingRequirements {
 
   type UniVarFunction = F32 => F32
 
+  type Vec2K[T] = (T, T)
   type Vec3K[T] = (T, T, T)
   type Vec3 = Vec3K[F32]
 
@@ -61,55 +62,52 @@ object __TracingRequirements {
     }
   }
 
-  object AutoGradObjective_GetTangentForm { // AKA exterior diff 1-form, maybe easier than gradient form
+  object AutoGradObjective_GetTangentForm {
 
-    trait LinearMixin {
-      self: (? => ?) =>
-    }
-    infix trait LinearApproximator[R, T] extends Function1[R, T] with LinearMixin {
+    /**
+      * A linear function/line/plane/hyperplane and a point on it, approximating the original function.
+      *
+      * Alternatively, this function can be uniquely derived from input/output and:
+      *   - a total diff (if output is a number) or a Jacobian (if output is a vector), to be accumulated in any order
+      *     (usually backward)
+      *
+      * AKA exterior diff/1-form/1st-order Taylor series, presumably easier to reason with than pure Jacobian
+      */
+    infix trait TangentApproximator[R, T] {
 
-      /**
-        * Alternatively, can be represented by:
-        *   - an input-output value pair, to be used in forward inference
-        *   - a total diff (if output is a number) or a Jacobian (if output is a vector), to be accumulated in any order
-        *     (usually backward)
-        */
-    }
-    object LinearApproximator {
-      def apply[R, T](f: R => T): R LinearApproximator T = {
-
-//        f.asInstanceOf[R LinearApproximator T]
-
-      }
+      val linearFn: Function1[R, T]
+      val in: R // <-- actually useless in autograd
+      lazy val out: T = linearFn(in) // <-- useful in forward inference
     }
 
-    /** given a function and a point, return an [[LinearApproximator]] near that point * */
-    type GetTangentElementary = UniVarFunction => F32 => LinearApproximator[F32, F32]
+    /** given a function and a point, return an [[TangentApproximator]] near that point * */
+    type GetTangentElementary = UniVarFunction => F32 => TangentApproximator[F32, F32]
     val getTangentElementary: GetTangentElementary = ???
 
-    //
-    type GetTangent = Vec3Function => Vec3 => LinearApproximator[Vec3, F32]
+    type GetTangent = Vec3Function => Vec3 => TangentApproximator[Vec3, F32]
     val getTangent: GetTangent = { throw new UnsupportedOperationException("WTF is this?") }
 
     trait Plain {
 
-      val linear: ((F32, F32, F32)) => Vec3Function & LinearMixin = getTangent(fn)
+      val linear: ((F32, F32, F32)) => TangentApproximator[Vec3, F32] = getTangent(fn)
       linear(1, 2, 3)
     }
 
     trait Compiled extends FusingObjective.Compiled {
 
-      val stage1Tangent: Option[(Vec3Function & LinearMixin, Vec3Function & LinearMixin)] = input.map {
+      val stage1Tangent: Option[Vec2K[TangentApproximator[Vec3, F32]]] = input.map {
         case (x, y, z) =>
 
           val f1Tangent = {
             val f1d = getTangentElementary(f1)(x)
 
-            LinearApproximator { (v: Vec3) =>
-              {
+            new TangentApproximator[Vec3, F32] {
+
+              override val linearFn: Vec3 => F32 = { (v: Vec3) =>
                 val (x, y, z) = v
-                f1d(x)
+                f1d.linearFn(x)
               }
+              override val in: (F32, F32, F32) = (x, y, z)
             }
           }
 
@@ -117,7 +115,7 @@ object __TracingRequirements {
             val f2dy = getTangentElementary((t: F32) => f2(t, z))(y)
             val f2dz = getTangentElementary((t: F32) => f2(y, t))(z)
 
-            LinearApproximator { (v: Vec3) =>
+            TangentApproximator { (v: Vec3) =>
               {
                 val (x, y, z) = v
                 f2dy(y) + f2dz(z)
@@ -131,9 +129,9 @@ object __TracingRequirements {
       val stage2Tangent: Option[((F32, F32, F32)) => F32] = stage1Tangent.map {
         case (t1, t2) =>
 
-          LinearApproximator { (v: Vec3) =>
+          TangentApproximator { (v: Vec3) =>
             t1(v) + t2(v) + 1 // <-- already linear, no need to getTangentElementary again
-            // if not, use the
+            // otherwise, use the output of
           }
       }
 
