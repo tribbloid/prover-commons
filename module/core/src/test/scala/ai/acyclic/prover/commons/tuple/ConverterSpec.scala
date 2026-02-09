cf9db3930c73c6d762a0c493628675f263faf928
pengcheng <peng.cheng@acyclic.ai>
2026-02-09 18:58:54 -0500

    add back

--- module/core/src/test/scala/ai/acyclic/prover/commons/tuple/ConverterSpec.scala
+++ module/core/src/test/scala/ai/acyclic/prover/commons/tuple/ConverterSpec.scala
@@ -18,5 +18,52 @@ class ConverterSpec extends BaseSpec {
     override val to: B2.type = B2
   }
 
-  describe("Converter") {}
+  describe("Converter") {
+
+    it("empty") {
+      val v1 = B1.Empty
+      val v2 = Conv(v1)
+      assert(v2 == B2.Empty)
+    }
+
+    it("single") {
+      //      import B1._
+      //      import B1.tupleOps
+
+      val v1 = 1 ><: B1.Empty
+      val v2 = Conv(v1)
+      assert(v2 == 1 ><: B2.Empty)
+    }
+
+    it("multiple") {
+
+      val v1 = 1 ><: "a" ><: B1.Empty
+      val v2 = Conv(v1)
+      assert(v2 == 1 ><: "a" ><: B2.Empty)
+
+    }
+  }
+
+  describe("Constrained") {
+
+    object B3 extends RecursiveHeapBackbone {
+      override type VBound = Int
+    }
+
+    object B4 extends RecursiveHeapBackbone {
+      override type VBound = Int
+    }
+
+    object Conv2 extends Converter {
+      override val from: B3.type = B3
+      override val to: B4.type = B4
+    }
+
+    it("pass") {
+      val v1 = 1 ><: B3.Empty
+      val v2 = Conv2(v1)
+      assert(v2 == 1 ><: B4.Empty)
+    }
+
+  }
 }
