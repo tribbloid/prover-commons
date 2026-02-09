490b7cf8929df0cd92276776a8b802f30acaea0e
pengcheng <peng.cheng@acyclic.ai>
2026-02-09 17:45:23 -0500

    pre! AI

--- module/core/src/main/scala/ai/acyclic/prover/commons/tuple/SchemaMixin.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/tuple/SchemaMixin.scala
@@ -1,9 +1,10 @@
 package ai.acyclic.prover.commons.tuple
 
 import ai.acyclic.prover.commons.jit.hom.Hom.Poly
+import shapeless.ops.hlist.Tupler
 
 import scala.language.implicitConversions
-import shapeless.{::, <:!<, =:!=, Generic, HList, HNil}
+import shapeless.{::, HList, HNil}
 
 trait SchemaMixin {
   self: BTuples =>
@@ -22,7 +23,7 @@ trait SchemaMixin {
     implicit def default[I <: self.Inductive, L <: HList, O](
         implicit
         toTuple: self.ToTuple.|-[I, L],
-        hlistToFlat: SchemaMixin.HListToFlat.Aux[L, O]
+        hlistToFlat: Tupler.Aux[L, O]
     ): I |- O = at[I] { i =>
       hlistToFlat(toTuple(i))
     }
@@ -35,51 +36,4 @@ trait SchemaMixin {
 
 }
 
-object SchemaMixin {
-
-  trait HListToFlat[L <: HList] {
-    type Out
-    def apply(l: L): Out
-  }
-
-  object HListToFlat {
-    type Aux[L <: HList, O] = HListToFlat[L] { type Out = O }
-
-    // Fix for HNil.type invariance in Tupler not needed with explicit cases
-
-    implicit def hnilCase[L <: HList](
-        implicit
-        ev: L <:< HNil
-    ): Aux[L, Unit] = new HListToFlat[L] {
-      type Out = Unit
-      def apply(l: L): Unit = ()
-    }
-
-    implicit def singleCase[H, T <: HList](
-        implicit
-        ev: T <:< HNil
-    ): Aux[H :: T, H] = new HListToFlat[H :: T] {
-      type Out = H
-      def apply(l: H :: T): H = l.head
-    }
-
-    // TODO: the following cases are not exhaustive, they should be merged into a single case defined inductively
-    implicit def tuple2Case[H1, H2, T <: HList](
-        implicit
-        ev: T <:< HNil
-    ): Aux[H1 :: H2 :: T, (H1, H2)] = new HListToFlat[H1 :: H2 :: T] {
-      type Out = (H1, H2)
-      def apply(l: H1 :: H2 :: T): (H1, H2) = (l.head, l.tail.head)
-    }
-
-    implicit def tuple3Case[H1, H2, H3, T <: HList](
-        implicit
-        ev: T <:< HNil
-    ): Aux[H1 :: H2 :: H3 :: T, (H1, H2, H3)] = new HListToFlat[H1 :: H2 :: H3 :: T] {
-      type Out = (H1, H2, H3)
-      def apply(l: H1 :: H2 :: H3 :: T): (H1, H2, H3) = (l.head, l.tail.head, l.tail.tail.head)
-    }
-
-    // fallback for larger tuples could use Tupler if we can fix the recursion, but for now explicit is safer.
-  }
-}
+object SchemaMixin {}
