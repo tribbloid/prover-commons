19ec9331e518448719ee24bda339b0fb6e853e4d
pengcheng <peng.cheng@acyclic.ai>
2026-02-10 00:35:52 -0500

    pre! AI

--- module/core/src/main/scala/ai/acyclic/prover/commons/tuple/FlatReprMixin.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/tuple/FlatReprMixin.scala
@@ -9,31 +9,6 @@ trait FlatReprMixin {
 
   import self._
 
-  trait ToHList[I <: self.Inductive] {
-    type Out <: HList
-    def apply(i: I): Out
-  }
-
-  object ToHList {
-    type Aux[I <: self.Inductive, O <: HList] = ToHList[I] { type Out = O }
-
-    implicit val empty: Aux[self.Empty, HNil] = new ToHList[self.Empty] {
-      type Out = HNil
-      def apply(i: self.Empty): HNil = HNil
-    }
-
-    implicit def cons[H <: self.VBound, T <: self.Inductive, TO <: HList](
-        implicit
-        tail: Aux[T, TO]
-    ): Aux[self.><:[H, T], H :: TO] = new ToHList[self.><:[H, T]] {
-      type Out = H :: TO
-      def apply(i: self.><:[H, T]): H :: TO = {
-        val (h, t) = self.deCons(i)
-        h :: tail(t)
-      }
-    }
-  }
-
   trait ToFlatRepr_Imp0 extends Poly {
 
     implicit def forTuples[I <: self.Inductive, L <: HList, O](
