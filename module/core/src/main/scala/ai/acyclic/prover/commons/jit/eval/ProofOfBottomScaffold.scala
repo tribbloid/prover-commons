b9d44e39e19745ad282c972cdf0440c7e801ef37
pengcheng <peng.cheng@acyclic.ai>
2026-03-03 15:54:01 -0500

    pre!

--- module/core/src/main/scala/ai/acyclic/prover/commons/jit/eval/ProofOfBottomScaffold.scala
+++ module/core/src/main/scala/ai/acyclic/prover/commons/jit/eval/ProofOfBottomScaffold.scala
@@ -2,16 +2,13 @@ package ai.acyclic.prover.commons.jit.eval
 
 object ProofOfBottomScaffold {
 
-  private def uncheckedProof[A, B]: A <:< B =
-    scala.Predef.$conforms[A].asInstanceOf[A <:< B]
-
   sealed trait TupleThing {
 
     type Peer >: this.type <: TupleThing
 
     type Bottom <: Peer
 
-    def proofOfBottom[TSub <: Peer]: Bottom <:< TSub
+    def proofOfBottom[TSub <: Peer](v: TSub): Bottom <:< TSub
   }
 
   case object Eye extends TupleThing {
@@ -20,7 +17,6 @@ object ProofOfBottomScaffold {
 
     override type Bottom = Eye.type
 
-    override def proofOfBottom[TSub <: Peer]: Bottom <:< TSub = uncheckedProof
   }
 
   type ><:[+H, +T <: TupleThing] = Cons[? <: H, ? <: T]
@@ -31,15 +27,5 @@ object ProofOfBottomScaffold {
 
     override type Bottom = ><:[Nothing, tail.Bottom] & Peer
 
-    override def proofOfBottom[TSub <: Peer]: Bottom <:< TSub = {
-      (tail: TupleThing) match {
-        case Eye =>
-          ()
-        case cons: Cons[_, _] =>
-          cons.proofOfBottom[cons.Peer]
-      }
-
-      uncheckedProof[Bottom, TSub]
-    }
   }
 }
